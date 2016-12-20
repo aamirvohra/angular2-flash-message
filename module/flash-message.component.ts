c58140b58ef36709c9747cd4f9e6ccf732bed4b0
aamirvohra <aamir_vohra@hotmail.com>
2016-12-20 14:39:11 -0500

    styles moved to external file

--- module/flash-message.component.ts
+++ module/flash-message.component.ts
@@ -2,6 +2,7 @@ import { Component, ElementRef, Input } from '@angular/core';
 import { FlashMessagePositioning, FlashMessageInterface } from './flash-message';
 import { FlashMessageService } from './flash-message.service';
 import { findIndex } from 'lodash';
+import { STYLES } from './styles';
 
 @Component({
   selector: 'flash-message',
@@ -18,52 +19,9 @@ import { findIndex } from 'lodash';
                 </div>
                </div>
             `,
-  styles: [`
-            .top-right {
-              position: fixed;
-              top: 75px;
-              right: 29px;
-              z-index: 1030;
-            }
-            
-            .alert {
-              min-width: 300px;
-              max-width: 350px;
-              min-height: 70px;
-              max-height: 100px;
-            }
-            
-            .alert > div {
-              padding: 7px;
-            }
-            
-            .alert .close {
-              position: absolute;
-              right: 8px;
-              top: auto;
-              margin-top: 2px;
-            }
-            
-            .alert .close:hover {
-              cursor: pointer;
-            }
-            
-            .alert-danger {
-              background-color: #E57373;
-            }
-          
-            .alert-warning {
-              background-color: #FFCC00;
-            }
-          
-            .alert-success {
-              background-color: #2ecc71;
-            }
-          
-            .alert-info {
-              background-color: #90CAF9;
-            }
-           `],
+  styles: [
+    STYLES
+  ],
 })
 
 export class FlashMessagesComponent {
